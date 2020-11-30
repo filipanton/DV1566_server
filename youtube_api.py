32af42a0a5baae023a68cdba8b972fd5d9089a9b
filipanton <filipanton@hotmail.com>
2020-11-30 13:47:42 +0100

    too many connections

---
+++ youtube_api.py
@@ -0,0 +1,28 @@
+import requests
+import mysql.connector
+
+# youtube key
+key = 'AIzaSyBpMyq8TYlXYhdzTZ26936WPPyy13b7hvQ'
+
+
+def connect():
+    cnx = mysql.connector.connect(user='sql7379267', password='rAuYPjlxvi',
+                                  host='sql7.freemysqlhosting.net',
+                                  database='sql7379267')
+    return cnx
+
+
+def get_trailers():
+    url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&order=relevance&q=trailer&regionCode=US&relevanceLanguage=EN&topicId=%2Fm%2F02vxn&type=video&key={}'.format(
+        key)
+    items = requests.get(url).json()['items']
+    cnx = connect()
+    cursor = cnx.cursor()
+    sql = "INSERT INTO trailers (videoId, title, published) VALUES (%s, %s, %s)"
+    for item in items:
+        val = (item['id']['videoId'], item['snippet']
+               ['title'], item['snippet']['publishedAt'])
+        cursor.execute(sql, val)
+    cursor.close()
+    cnx.close()
+    print("1 hour")
